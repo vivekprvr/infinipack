9f554e77837af0b4b7aeb5e39de2c3fcb27983f3
rmvivekprvr <vivek@roombr.com>
2023-08-12 03:05:22 +0530

    ui text option removed

--- js/gsap.js
+++ js/gsap.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const elements = document.querySelectorAll('.move-element');
     const animateOnloadFromTop = document.querySelector('.animate-onload-from-top');
     const animateOnloadFromBottom = document.querySelector('.animate-onload-from-bottom');
+    const fadeInElement = document.querySelector('.fade-in');
 
     // Mousemove-based animation for multiple elements using GSAP
     document.addEventListener('mousemove', (event) => {
@@ -13,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
           x: (mouseX - window.innerWidth / 2) * 0.02,
           y: (mouseY - window.innerHeight / 2) * 0.02,
           duration: 0.5,
-          ease: 'power2.out'
+          ease: 'easeInOut'
         });
       });
     });
@@ -23,18 +24,24 @@ document.addEventListener("DOMContentLoaded", function() {
       opacity: 0,
       y: -100,
       duration: 1,
-      ease: 'power2.out'
+      ease: 'easeInOut'
     });
 
     gsap.from(animateOnloadFromBottom, {
         opacity: 0,
-        y: 800,
+        y: 100,
         duration: 1.5,           // Increase duration for a smoother transition
-        ease: 'power4.out'       // Use a smoother easing function
+        ease: 'easeInOut'       // Use a smoother easing function
       });
 
-    // Restart timeline animation on button click
-    document.getElementById("animate").onclick = function () {
-      tl.restart();
-    };
+          // Set initial opacity to 0
+    gsap.set(fadeInElement, { opacity: 0 });
+
+    // Fade in animation using GSAP
+    gsap.to(fadeInElement, {
+      opacity: 1,        // Target opacity of 1 (fully visible)
+      duration: 3,       // Animation duration in seconds
+      ease: 'easeInOut' // Easing function
+    });
+
   });
